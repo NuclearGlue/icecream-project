1cda8c55445f4d34407b09c45b0afe79521ca761
NuclearGlue <sanek1678@gmail.com>
2021-09-11 08:32:53 +0000

    Deploying to gh-pages from @ NuclearGlue/icecream-project@8fba9e813177d3222016783dcd8f9f5c2aa1eb9d 🚀

--- src.bffb0a2c.js
+++ src.bffb0a2c.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/icecream-project/src.e4ac0d0b.js.map
+//# sourceMappingURL=/icecream-project/src.bffb0a2c.js.map
